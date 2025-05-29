03f858591684d30c73761424d1a17283113030fe
Mololuwa Segilola <72552301+mololuwa10@users.noreply.github.com>
2025-05-29 14:36:56 +0100

    `Added new API endpoints for getting journals and journal entries by ID, including new properties in the JournalEntry interface.`

--- lib/apiGetActions.tsx
+++ lib/apiGetActions.tsx
@@ -18,6 +18,8 @@ export interface JournalEntry {
 	journalId: string;
 	title: string;
 	content: string;
+	mood: "VerySad" | "Sad" | "Neutral" | "Happy" | "VeryHappy";
+	location: string;
 	dateCreated: string;
 	dateModified: string;
 	journalEntryTags: JournalEntryTag[];
@@ -38,6 +40,8 @@ const ip =
 	Constants.manifest2?.extra?.apiHost ||
 	"localhost";
 
+// JOURNAL =================================================================================================
+// GET ALL JOURNALS
 export const getJournals = async (): Promise<Journal[]> => {
 	try {
 		const token = await AsyncStorage.getItem("token");
@@ -65,5 +69,71 @@ export const getJournals = async (): Promise<Journal[]> => {
 	} catch (error: any) {
 		console.error("There was an error getting journals: ", error);
 		return [];
+	}
+};
+
+// GET ALL JOURNALS BY ID
+export const getJournalById = async (
+	journalId: string
+): Promise<Journal | null> => {
+	try {
+		const token = await AsyncStorage.getItem("token");
+		if (!token) throw new Error("No token found");
+
+		const response = await fetch(`http://${ip}:5183/api/journal/${journalId}`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+		});
+
+		if (!response.ok) {
+			const errorMsg = await response.text();
+			console.error("Failed to fetch journal by id:", errorMsg);
+			return null;
+		}
+
+		const raw = await response.json();
+		const data: Journal | null = raw || null;
+		return data;
+	} catch (error: any) {
+		console.error("There was an error getting journal by id: ", error);
+		return null;
+	}
+};
+// =============================================================================
+
+// Journal Entries ==============================================================
+// Get Journal Entry by Entry Id
+export const getJournalEntryById = async (
+	entryId: string
+): Promise<JournalEntry | null> => {
+	try {
+		const token = await AsyncStorage.getItem("token");
+		if (!token) throw new Error("No token found");
+
+		const response = await fetch(
+			`http://${ip}:5183/api/journalentry/${entryId}`,
+			{
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+			}
+		);
+
+		if (!response.ok) {
+			const errorMsg = await response.text();
+			console.error("Failed to fetch journal entry by id:", errorMsg);
+			return null;
+		}
+
+		const data: JournalEntry = await response.json();
+		return data;
+	} catch (error: any) {
+		console.error("There was an error getting journal entry by id: ", error);
+		return null;
 	}
 };
