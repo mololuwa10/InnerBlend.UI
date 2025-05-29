728b83f5b0fa1b41b6dfbf39086b2ff7cccb6e4e
Mololuwa Segilola <72552301+mololuwa10@users.noreply.github.com>
2025-05-29 15:34:14 +0100

    `Added a new API endpoint for deleting journal entries and a function for deleting journals, along with error handling and toast notifications.`

--- lib/apiDeleteActions.tsx
+++ lib/apiDeleteActions.tsx
@@ -44,3 +44,91 @@ export const deleteUser = async () => {
 		throw new Error("Failed to delete the user. Please try again.");
 	}
 };
+
+// JOURNAL ENTRIES ================================================================
+// DELETE JOURNAL ENTRY
+export const deleteJournalEntry = async (
+	entryId: string
+): Promise<boolean | null> => {
+	const token = await AsyncStorage.getItem("token");
+	if (!token) throw new Error("No token found");
+
+	try {
+		const response = await fetch(
+			`http://${ip}:5183/api/journalentry/${entryId}`,
+			{
+				method: "DELETE",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+			}
+		);
+
+		if (response.ok) {
+			Toast.show({
+				type: "success",
+				text1: "Entry Deleted",
+				text2: "Your journal entry was successfully removed.",
+			});
+			return true;
+		} else {
+			const errorText = await response.text();
+			Toast.show({
+				type: "error",
+				text1: "Delete Failed",
+				text2: errorText || "Something went wrong.",
+			});
+			return false;
+		}
+	} catch (error: any) {
+		console.error("Error deleting journal entry:", error.message);
+		Toast.show({
+			type: "error",
+			text1: "Network Error",
+			text2: "Failed to delete journal entry. Please try again.",
+		});
+		return false;
+	}
+};
+// ==========================================================================
+
+// JOURNAL ====================================================================
+export const deleteJournal = async (
+	journalId: string
+): Promise<boolean | null> => {
+	try {
+		const token = await AsyncStorage.getItem("token");
+		if (!token) throw new Error("No token found");
+
+		const response = await fetch(`http://${ip}:5183/api/journal/${journalId}`, {
+			method: "DELETE",
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+		});
+
+		if (!response.ok) {
+			const errorText = await response.text();
+			console.error("Failed to delete journal:", errorText);
+			Toast.show({
+				type: "error",
+				text1: "Delete Failed",
+				text2: "Could not delete journal. Try again.",
+			});
+			return false;
+		}
+
+		Toast.show({
+			type: "success",
+			text1: "Deleted",
+			text2: "Journal has been successfully deleted.",
+		});
+
+		return true;
+	} catch (error: any) {
+		console.error("There was an error deleting journal: ", error);
+		return null;
+	}
+};
